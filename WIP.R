134fb23e4a0173ed5f56eb6b93d2b80ef9050518
Ankur Verma <ankurv857@gmail.com>
2016-08-12 15:15:27 +0530

    Update WIP.R

--- WIP.R
+++ WIP.R
@@ -1,32 +1,27 @@
 
-#function for EDA (bivariate)
+setwd("~/Competitions/DS")
+getwd()
 
-#create data frame
-a = c(23,45,12,56,34,78,98,65,43,32,65,85,52,54,65)
-b = c(2,3,4,3,5,6,8,2,9,7,6,5,4,2,3)
-d = c('a','w','w','a','w','a','w','s','w','a','s','w','s','a','s')
+library(dplyr)
 
-c = data.frame(feature = a, dependent = b)
+#train = read_csv("train.csv")
+head(train)
 
-q = data.frame(perc = seq(0,1,0.1), bucket_value = quantile(c[,1],probs  = seq(0,1,0.1)))
+sam = train
 
+colnames(sam)[2] = "birth_year"
+colnames(sam)[3] = "permit_year"
+colnames(sam)[4] = "mark"
+colnames(sam)[5] = "tax_then"
 
-for ( i in 1: nrow(c))
-{
-  for (j in 1 : nrow(q))
-  {
-     if(q[j,2] <= c[i,1] < q[j+1,2] ) q[j,1] else 0
-    
-  }
-}
-  
+#feature engineering
+head(sam)
 
-for ( i in 1: nrow(c))
-{
-  for (j in 1 : nrow(q))
-  {
-    if(c[i,1] == q[j+1,2])
-      c$bucket = q[j,1] else 
-      c$bucket =   0
-  }
-}
+f = sam[c(3,34)]
+
+sum(is.na(f[c(1)]))
+
+f$bucket = ntile(f$permit_year,10)
+roll = aggregate(prime_tot_ttc ~ bucket , data = f,FUN = mean)
+plot(roll$bucket,roll$prime_tot_ttc)
+
