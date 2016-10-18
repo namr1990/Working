4170c572b1ae37f89027dbeb38b944e98f7a7eb3
Ankur Verma <ankurv857@gmail.com>
2016-10-19 00:22:00 +0530

    Update Hackerearth.R

--- Hackerearth.R
+++ Hackerearth.R
@@ -35,38 +35,224 @@ for (i in 3:101)
   data = rbind(data,data1)
 }
 
+data = transp
+
 ## Preprocess using caret 
 
-limitedTrain = data[c(2:9)]
+limitedTrain = data
 preproc = preProcess(limitedTrain)
 normTrain = predict(preproc, limitedTrain)
 
-km <- kmeans(normTrain, centers=14)
+km <- kmeans(normTrain, centers=7)
 
 ## Generate classification for train and test sets
 km.kcca = as.kcca(km, normTrain)
 clusterTrain = predict(km.kcca)
 table(clusterTrain)
 
-check = subset(limitedTrain, clusterTrain == 2)
+check = subset(limitedTrain, clusterTrain == 1)
 View(check)
 
 check1 = cbind(limitedTrain,clusterTrain)
 View(check1)
 
-qwe = check1 %>% group_by(clusterTrain) %>% summarise(A_mean = mean(A),
-                                                      B_mean = mean(B),
-                                                      C_mean = mean(C),
-                                                      D_mean = mean(D),
-                                                      E_mean = mean(E),
-                                                      F_mean = mean(F),
-                                                      G_mean = mean(G),
-                                                      R_mean = mean(R_Square), count = n())
-                                                    
-qwe = check1 %>% group_by(clusterTrain) %>% summarise(
-                                                      R_mean = mean(R_Square), count = n())
+asd = check1[c(4362)]
+asd1 = data.frame(Asset = 1:nrow(asd), asd)
+asd2 = data.frame(Asset = paste0("X",asd1$Asset), Cluster = asd1$clusterTrain)
+write.csv(asd2,"asd2.txt",row.names = FALSE)
 
-asd = check1[c(9)]
+#transposed dataset
+
+transp = t(tt)
+transp = data.frame(transp)
+transp  = transp[2:101,]
+
+apply(transp,2,var)
+pca = prcomp(transp)
+plot(pca)
+biplot (pca , scale =0)
+
+#correlation
+
+ttc = tt
+ttc$Time = NULL
+asd = data.frame(cor(ttc))
+
+
+wer = asd
+
+for (i in 1:100)
+  {
+  for (j in 1:100)
+    {
+       if(wer[i,j] < 0.9)
+       {
+         wer[i,j] = NA
+       }
+    }
+  }
+
+
+
+for (i in 1:100)
+{
+  for (j in 1:100)
+  {
+    wer[i,j][(!is.na(wer[i,j]))] = j
+  }
+} 
+
+
+
+write.csv(wer,"wer.csv",row.names = FALSE)  
+
+#percentile based slopes
+
+
+PN = data.frame(TIME =tt$Time,X = tt[,2], PT = ntile(tt[,2],10))
+
+sub = subset(PN, PN$PT == 1)
+as = lm(formula = sub$X ~ poly(sub$TIME,1,raw = TRUE))
+X1 =as.data.frame(summary(as)$coef)
+data = data.frame(ID = 1,M1 =  X1[2,1])
+
+sub = subset(PN, PN$PT == 2)
+as = lm(formula = sub$X ~ poly(sub$TIME,1,raw = TRUE))
+X1 =as.data.frame(summary(as)$coef)
+data1 = data.frame(ID = 1,M2 =  X1[2,1])
+data = cbind(data,M2 = data1$M2)
+
+sub = subset(PN, PN$PT == 3)
+as = lm(formula = sub$X ~ poly(sub$TIME,1,raw = TRUE))
+X1 =as.data.frame(summary(as)$coef)
+data1 = data.frame(ID = 1,M3 =  X1[2,1])
+data = cbind(data,M3 = data1$M3)
+
+sub = subset(PN, PN$PT == 4)
+as = lm(formula = sub$X ~ poly(sub$TIME,1,raw = TRUE))
+X1 =as.data.frame(summary(as)$coef)
+data1 = data.frame(ID = 1,M4 =  X1[2,1])
+data = cbind(data,M4 = data1$M4)
+
+sub = subset(PN, PN$PT == 5)
+as = lm(formula = sub$X ~ poly(sub$TIME,1,raw = TRUE))
+X1 =as.data.frame(summary(as)$coef)
+data1 = data.frame(ID = 1,M5 =  X1[2,1])
+data = cbind(data,M5 = data1$M5)
+
+sub = subset(PN, PN$PT == 6)
+as = lm(formula = sub$X ~ poly(sub$TIME,1,raw = TRUE))
+X1 =as.data.frame(summary(as)$coef)
+data1 = data.frame(ID = 1,M6 =  X1[2,1])
+data = cbind(data,M6 = data1$M6)
+
+sub = subset(PN, PN$PT == 7)
+as = lm(formula = sub$X ~ poly(sub$TIME,1,raw = TRUE))
+X1 =as.data.frame(summary(as)$coef)
+data1 = data.frame(ID = 1,M7 =  X1[2,1])
+data = cbind(data,M7 = data1$M7)
+
+sub = subset(PN, PN$PT == 8)
+as = lm(formula = sub$X ~ poly(sub$TIME,1,raw = TRUE))
+X1 =as.data.frame(summary(as)$coef)
+data1 = data.frame(ID = 1,M8 =  X1[2,1])
+data = cbind(data,M8 = data1$M8)
+
+sub = subset(PN, PN$PT == 9)
+as = lm(formula = sub$X ~ poly(sub$TIME,1,raw = TRUE))
+X1 =as.data.frame(summary(as)$coef)
+data1 = data.frame(ID = 1,M9 =  X1[2,1])
+data = cbind(data,M9 = data1$M9)
+
+sub = subset(PN, PN$PT == 10)
+as = lm(formula = sub$X ~ poly(sub$TIME,1,raw = TRUE))
+X1 =as.data.frame(summary(as)$coef)
+data1 = data.frame(ID = 1,M10 =  X1[2,1])
+data = cbind(data,M10 = data1$M10)
+data2 = data
+
+for ( i in 3:101)
+{
+  
+  PN = data.frame(TIME =tt$Time,X = tt[,i], PT = ntile(tt[,i],10))
+  
+  sub = subset(PN, PN$PT == 1)
+  as = lm(formula = sub$X ~ poly(sub$TIME,1,raw = TRUE))
+  X1 =as.data.frame(summary(as)$coef)
+  data = data.frame(ID = i,M1 =  X1[2,1])
+  
+  sub = subset(PN, PN$PT == 2)
+  as = lm(formula = sub$X ~ poly(sub$TIME,1,raw = TRUE))
+  X1 =as.data.frame(summary(as)$coef)
+  data1 = data.frame(ID = i,M2 =  X1[2,1])
+  data = cbind(data,M2 = data1$M2)
+  
+  sub = subset(PN, PN$PT == 3)
+  as = lm(formula = sub$X ~ poly(sub$TIME,1,raw = TRUE))
+  X1 =as.data.frame(summary(as)$coef)
+  data1 = data.frame(ID = i,M3 =  X1[2,1])
+  data = cbind(data,M3 = data1$M3)
+  
+  sub = subset(PN, PN$PT == 4)
+  as = lm(formula = sub$X ~ poly(sub$TIME,1,raw = TRUE))
+  X1 =as.data.frame(summary(as)$coef)
+  data1 = data.frame(ID = i,M4 =  X1[2,1])
+  data = cbind(data,M4 = data1$M4)
+  
+  sub = subset(PN, PN$PT == 5)
+  as = lm(formula = sub$X ~ poly(sub$TIME,1,raw = TRUE))
+  X1 =as.data.frame(summary(as)$coef)
+  data1 = data.frame(ID = i,M5 =  X1[2,1])
+  data = cbind(data,M5 = data1$M5)
+  
+  sub = subset(PN, PN$PT == 6)
+  as = lm(formula = sub$X ~ poly(sub$TIME,1,raw = TRUE))
+  X1 =as.data.frame(summary(as)$coef)
+  data1 = data.frame(ID = i,M6 =  X1[2,1])
+  data = cbind(data,M6 = data1$M6)
+  
+  sub = subset(PN, PN$PT == 7)
+  as = lm(formula = sub$X ~ poly(sub$TIME,1,raw = TRUE))
+  X1 =as.data.frame(summary(as)$coef)
+  data1 = data.frame(ID = i,M7 =  X1[2,1])
+  data = cbind(data,M7 = data1$M7)
+  
+  sub = subset(PN, PN$PT == 8)
+  as = lm(formula = sub$X ~ poly(sub$TIME,1,raw = TRUE))
+  X1 =as.data.frame(summary(as)$coef)
+  data1 = data.frame(ID = i,M8 =  X1[2,1])
+  data = cbind(data,M8 = data1$M8)
+  
+  sub = subset(PN, PN$PT == 9)
+  as = lm(formula = sub$X ~ poly(sub$TIME,1,raw = TRUE))
+  X1 =as.data.frame(summary(as)$coef)
+  data1 = data.frame(ID = i,M9 =  X1[2,1])
+  data = cbind(data,M9 = data1$M9)
+  
+  sub = subset(PN, PN$PT == 10)
+  as = lm(formula = sub$X ~ poly(sub$TIME,1,raw = TRUE))
+  X1 =as.data.frame(summary(as)$coef)
+  data1 = data.frame(ID = i,M10 =  X1[2,1])
+  data = cbind(data,M10 = data1$M10)
+  
+  data2 = rbind(data2,data)
+}
+
+normTrain = data2[-c(1)]
+km <- kmeans(normTrain, centers=15)
+
+## Generate classification for train and test sets
+km.kcca = as.kcca(km, normTrain)
+clusterTrain = predict(km.kcca)
+table(clusterTrain)
+
+check = subset(limitedTrain, clusterTrain == 1)
+View(check)
+
+check1 = cbind(limitedTrain,clusterTrain)
+View(check1)
+
+asd = check1[c(4362)]
 asd1 = data.frame(Asset = 1:nrow(asd), asd)
 asd2 = data.frame(Asset = paste0("X",asd1$Asset), Cluster = asd1$clusterTrain)
 write.csv(asd2,"asd2.txt",row.names = FALSE)
